3c2e4e744c05a157e5714473249a663318db0182
ALexisDRZN <tl22590043@sjuanrio.tecnm.mx>
2025-10-10 18:37:39 -0600

    agregacion de platilla v2

---
+++ default/app/controllers/tablas_controller.php
@@ -0,0 +1,9 @@
+<?php
+class TablasController extends AppController
+{
+    public function index()
+    {
+        // Si quieres, puedes pasar datos desde el modelo a la vista
+        // $this->registros = Tabla::todos();
+    }
+}
