4717ce220614cc6442338682cedfc31734673d55
ALexisDRZN <tl22590043@sjuanrio.tecnm.mx>
2025-10-10 20:12:10 -0600

    agregacion de platilla v4

---
+++ default/app/controllers/reportes_controller.php
@@ -0,0 +1,10 @@
+<?php
+class ReportesController extends AppController
+{
+    public function index()
+    {
+        // Puedes preparar aquí los datos para las gráficas si los necesitas
+        // Por ejemplo:
+        // $this->datos = Grafica::datos();
+    }
+}
